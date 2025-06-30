7c4a0a01ac788e47ea9c35c4f87926592c0c0846
RosaBen <benchabane.rosa@outlook.com>
2025-06-30 10:35:01 +0200

    exo 2 and 3 done

--- script.js
+++ script.js
@@ -20,9 +20,49 @@ function footerClick() {
 function countFooterClic() {
   const footer = footerClick();
   let count = 0;
+  // si footer existe, compte le nombre de clics
   if (footer) {
     count++;
     console.log(`clic numéro ${count}`);
   };
 }
 countFooterClic();
+
+
+// Fonctionnalité 2
+// clique sur ce bouton, l'élément HTML portant l'Id navbarHeader perde sa classe collapse. Une fois que ça, ça marche, fait que si on clique à nouveau dessus, la classe collapse soit rajoutée à nouveau à l'élément portant l'Id navbarHeader
+
+function toggleNavbar() {
+  // sélectionne id du navbar
+  const navbar = document.getElementById("navbarHeader");
+  // sélectionne le bouton de la navbar
+  const button = document.querySelector(".navbar-toggler");
+  // si navbar et button existenet, cliquer pour cacher ou faire apparaitre les informations
+  if (navbar && button) {
+    button.addEventListener("click", function () {
+      navbar.classList.toggle("collapse");
+    });
+  }
+}
+
+toggleNavbar();
+
+// Fonctionnalité 3
+// clique sur le bouton "Edit" de la première card, le texte de la card va se mettre en rouge de façon irréversible (sauf si on recharge la page)
+
+function editHTML() {
+  // sélectionne le bouton edit
+  const buttonCard1 = document.querySelector(".btn-group:first-of-type");
+  // console.log("sélect:", buttonCard1)
+  const buttonEdit = buttonCard1.querySelector("button:nth-of-type(2)");
+  // console.log("edit", buttonEdit);
+  if (buttonEdit) {
+    buttonEdit.addEventListener("click", function () {
+      // console.log("button edit cliqué")
+      // ajoute text en rouge avec bootstrap
+      buttonEdit.classList.add("text-danger")
+    })
+  }
+}
+
+editHTML();
